b9a99a26609d7eee34540047973aa78087125822
arpollio <arpollio@gmail.com>
2019-09-13 10:39:00 -0400

    added a tentative raxml file with only the treeprep stuff

---
+++ Directions/raxml_pipeline.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+use strict;
+
+my $progname = $0;
+$progname =~ s/^.*?([^\/]+)$/$1/;
+
+# align each set of sequences and sort alignments by taxonomy
+FILES=*.fasta
+for f in $FILES
+do
+	fbase=${f%.fasta}
+
+  # convert the headers to taxonomy strings
+	fa_headmap.pl -tax < "$f" > "$fbase.mapped.fa"
+
+  # align the sequences
+	muscle -in "$fbase.mapped.fa" -verbose -out "$fbase.unsorted.afa"
+
+  # extract the taxonomy strings to a common file
+	grep '^>' "$fbase.unsorted.afa" >> "$taxonlist"
+done
